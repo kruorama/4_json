39ea8060585adc830b56213d379a00cc69db617a
Aleksandra Katonina <aleksandra.katonina@klarna.com>
2018-08-31 22:43:22 +0200

    Fixed PEP8

--- pprint_json.py
+++ pprint_json.py
@@ -1,15 +1,20 @@
 import json
 import sys
 
+
 filepath = sys.argv[1]
+
 
 def load_data(filepath):
     with open(filepath, 'r') as file_handler:
         return json.load(file_handler)
 
+
 json_decoded = load_data(filepath)
 
+
 def pretty_print_json(data):
-    print (json.dumps(data, indent=4, ensure_ascii=False))
+    print(json.dumps(data, indent=4, ensure_ascii=False))
+
 
 pretty_print_json(json_decoded)
