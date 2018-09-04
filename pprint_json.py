c521ff2a96cae7a7ace05605f8043e015a1ca4fb
Aleksandra Katonina <aleksandra.katonina@klarna.com>
2018-09-04 10:59:11 +0200

    Added exception type Removed exit() from function, now it returns None if it gets not json file

--- pprint_json.py
+++ pprint_json.py
@@ -3,17 +3,26 @@ import sys
 
 
 def pretty_print_json(filepath):
+
     try:
         with open(filepath, 'r') as file_handler:
             json_decoded = json.load(file_handler)
-    except Exception:
-        exit(print("It's not a valid JSON"))
-
-    print(json.dumps(json_decoded, indent=4, ensure_ascii=False))
+    except json.decoder.JSONDecodeError:
+        return None
+    """
+    with open(filepath, 'r') as file_handler:
+            json_decoded = json.load(file_handler)
+    """
+    pretty_json = json.dumps(json_decoded, indent=4, ensure_ascii=False)
+    return pretty_json
 
 
 if __name__ == '__main__':
     if len(sys.argv) <= 1:
         exit(print('Please add a path to JSON file'))
 
-    pretty_print_json(sys.argv[1])
+    pretty_json = pretty_print_json(sys.argv[1])
+    if pretty_json == None:
+        print("It's not a valid JSON")
+    else:
+        print(pretty_json)
